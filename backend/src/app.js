91edec0ec1a954ed5723ca6a01f1dfa4bb2a4d98
Satyam Raj <smritisoni@DODO-20.local>
2026-06-23 23:56:48 +0530

    added vercel.jsonl for preventing the SPA routing error, version  V3.3.2

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, 'https://vardaansolutions.vercel.app', 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL, 'https://vardaansolutions.vercel.app', 'https://vardaansolutions.onrender.com', 'http://localhost:5173'],
     credentials: true,
   })
 );
